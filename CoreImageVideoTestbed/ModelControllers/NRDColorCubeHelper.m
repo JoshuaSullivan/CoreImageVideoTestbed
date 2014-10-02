8452449be5de0168accf6f950cada7f6cd879eb1
Joshua Sullivan <jsulliva@nerdery.com>
2014-10-02 15:31:38 -0500

    Improved code comments.

--- CoreImageVideoTestbed/ModelControllers/NRDColorCubeHelper.m
+++ CoreImageVideoTestbed/ModelControllers/NRDColorCubeHelper.m
@@ -39,16 +39,18 @@
     // Copy the data and get a pointer.
     CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
 
+    // Get a pointer to the copied data.
     const void *inBuffer = CFDataGetBytePtr(inBitmapData);
 
+    // Calculate the size of the float buffer and allocate it.
     size_t floatSize = pixels * channels * sizeof(float);
     float *finalBuffer = malloc(floatSize);
-    float divisor = 255.0f;
 
-    // Convert the uint_8t to float
+    // Convert the uint_8t to float. Note: a uint of 255 will convert to 255.0f.
     vDSP_vfltu8(inBuffer, 1, finalBuffer, 1, memSize);
 
-    // Divide each float by 255 to get the 0-1 range we are looking for.
+    // Divide each float by 255.0 to get the 0-1 range we are looking for.
+    float divisor = 255.0f;
     vDSP_vsdiv(finalBuffer, 1, &divisor, finalBuffer, 1, memSize);
 
     // Don't copy the bytes, just have the NSData take ownership of the buffer.
